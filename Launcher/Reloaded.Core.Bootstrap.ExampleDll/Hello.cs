326e4273d7a91ba49bffb92dea2cfe8ccb812e68
trevis <trevis@haxit.org>
2024-09-21 15:59:29 -0600

    wip

--- Launcher/Reloaded.Core.Bootstrap.ExampleDll/Hello.cs
+++ Launcher/Reloaded.Core.Bootstrap.ExampleDll/Hello.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.DirectX.Direct3D;
+﻿using Reloaded.Core.Bootstrap.ExampleDll.Lib;
 using Reloaded.Hooks;
 using Reloaded.Hooks.Definitions;
+using Reloaded.Hooks.Definitions.Structs;
 using Reloaded.Hooks.Definitions.X86;
+using SharpDX.Direct3D9;
 using System;
 using System.Diagnostics;
 using System.IO;
@@ -9,59 +11,62 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
-using System.Security.Permissions;
+using System.Windows.Forms;
 
 namespace Reloaded.Core.Bootstrap.ExampleDll
 {
     public static class Hello
     {
-        private static IHook<RenderDeviceD3D__Init> RenderDeviceD3D_InitHook;
+        private static IHook<CreateDevice> _createDeviceHook;
+
+        public static IVirtualFunctionTable Direct3D9VTable { get; private set; }
+        public static IVirtualFunctionTable DeviceVTable { get; private set; }
+        public static Device D3Ddevice { get; private set; }
 
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
 
-        [Function(Hooks.Definitions.X86.CallingConventions.MicrosoftThiscall)]
-        public delegate void RenderDeviceD3D__Init(IntPtr thisPtr);
+        [Function(Hooks.Definitions.X86.CallingConventions.Stdcall)]
+        public delegate IntPtr CreateDevice(IntPtr a, uint b, DeviceType c, IntPtr d, CreateFlags e, IntPtr f, IntPtr g);
 
         public static int SayHello(IntPtr stringPtr, int size)
         {
-            RenderDeviceD3D_InitHook = ReloadedHooks.Instance.CreateHook<RenderDeviceD3D__Init>(RenderDeviceD3D__Init__Impl, 0x005A2BA0).Activate();
+            using var direct3D = new Direct3D();
+            using var renderForm = new Form();
+            using var device = new Device(direct3D, 0, DeviceType.Hardware, IntPtr.Zero, CreateFlags.HardwareVertexProcessing, GetParameters(direct3D, renderForm.Handle));
+
+            Direct3D9VTable = ReloadedHooks.Instance.VirtualFunctionTableFromObject(direct3D.NativePointer, Enum.GetNames(typeof(IDirect3D9)).Length);
+            DeviceVTable = ReloadedHooks.Instance.VirtualFunctionTableFromObject(device.NativePointer, Enum.GetNames(typeof(IDirect3DDevice9)).Length);
+            
+            var createDevicePtr = (long)Direct3D9VTable[(int)IDirect3D9.CreateDevice].FunctionPointer;
+            _createDeviceHook = ReloadedHooks.Instance.CreateHook<CreateDevice>(typeof(Hello), nameof(CreateDeviceImpl), createDevicePtr).Activate();
+
             return 0;
         }
 
-
-        private unsafe static void RenderDeviceD3D__Init__Impl(IntPtr thisPtr)
+        [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
+        private unsafe static IntPtr CreateDeviceImpl(IntPtr a, uint b, DeviceType c, IntPtr d, CreateFlags e, IntPtr f, IntPtr g)
         {
+            var devicePtr = _createDeviceHook.OriginalFunction.Invoke(a, b, c, d, e, f, g);
+            D3Ddevice = new Device(*((int*)g));
+            MessageBox(0, $"CreateDevice: {D3Ddevice.Viewport.Width},{D3Ddevice.Viewport.Height}", "Hello", 0);
 
-            RenderDeviceD3D_InitHook.OriginalFunction(thisPtr);
-            //var deviceId = *(int*)(thisPtr + 0x468);
-            //int* d3dDevicePtr = (int*)((*(int*)0x008F297C) + 0x480);
-            //File.AppendAllText(@"D:\projects\ACUI\bin\log.txt", $"deviceId: {(int)thisPtr:X8} {(int)*d3dDevicePtr:X8}\n");
-            //var D3Ddevice = new Device((int)d3dDevicePtr);
-
-            //File.AppendAllText(@"D:\projects\ACUI\bin\log.txt", $"RenderDeviceD3D__Init: {D3Ddevice.Viewport.Width},{D3Ddevice.Viewport.Height}\n");
-
-
-            //return res;
+            return devicePtr;
         }
 
-        private static string GetRuntimeVersion()
+        private static PresentParameters GetParameters(Direct3D d3d, IntPtr windowHandle)
         {
-            string framework = Assembly.GetExecutingAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
-            return framework;
-        }
-
-        private static string GetArchitecture()
-        {
-            switch (IntPtr.Size)
+            var mode = d3d.GetAdapterDisplayMode(0);
+            return new PresentParameters()
             {
-                case 4:
-                    return "x86";
-                case 8:
-                    return "x64";
-                default:
-                    return "The future is now";
-            }
+                BackBufferWidth = mode.Width,
+                BackBufferHeight = mode.Height,
+                BackBufferFormat = mode.Format,
+                DeviceWindowHandle = windowHandle,
+                SwapEffect = SwapEffect.Discard,
+                Windowed = true,
+                PresentationInterval = PresentInterval.Immediate
+            };
         }
     }
 }
---
+++ Launcher/Reloaded.Core.Bootstrap.ExampleDll/Lib/IDirect3D9.cs
@@ -0,0 +1,62 @@
+﻿namespace Reloaded.Core.Bootstrap.ExampleDll.Lib
+{
+    /// <summary>
+    /// Contains the D3D9 interface.
+    /// </summary>
+    public enum IDirect3D9
+    {
+        /*** IUnknown methods ***/
+        QueryInterface,
+        AddRef,
+        Release,
+
+        /*** IDirect3D9 methods ***/
+        RegisterSoftwareDevice,
+        GetAdapterCount,
+        GetAdapterIdentifier,
+        GetAdapterModeCount,
+        EnumAdapterModes,
+        GetAdapterDisplayMode,
+        CheckDeviceType,
+        CheckDeviceFormat,
+        CheckDeviceMultiSampleType,
+        CheckDepthStencilMatch,
+        CheckDeviceFormatConversion,
+        GetDeviceCaps,
+        GetAdapterMonitor,
+        CreateDevice
+    }
+    public unsafe struct BlittablePtr<T> where T : unmanaged
+    {
+        /// <summary>
+        /// The pointer to the value.
+        /// </summary>
+        public T* Pointer { get; set; }
+
+        /// <summary>
+        /// Creates a blittable pointer
+        /// </summary>
+        /// <param name="pointer"></param>
+        public BlittablePtr(T* pointer) => Pointer = pointer;
+
+        /// <summary/>
+        public static implicit operator BlittablePtr<T>(T* operand) => new BlittablePtr<T>(operand);
+    }
+
+    public unsafe struct BlittablePtrPtr<T> where T : unmanaged
+    {
+        /// <summary>
+        /// The pointer to the value.
+        /// </summary>
+        public T** Pointer { get; set; }
+
+        /// <summary>
+        /// Creates a blittable pointer
+        /// </summary>
+        /// <param name="pointer"></param>
+        public BlittablePtrPtr(T** pointer) => Pointer = pointer;
+
+        /// <summary/>
+        public static implicit operator BlittablePtrPtr<T>(T** operand) => new BlittablePtrPtr<T>(operand);
+    }
+}
