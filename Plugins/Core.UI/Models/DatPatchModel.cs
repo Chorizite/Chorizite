0d6ef424c690dede6f37b264539e64e0e2064f09
trevis <trevis@haxit.org>
2024-09-29 01:36:20 -0600

    cleanup

--- Plugins/Core.UI/Models/DatPatchModel.cs
+++ Plugins/Core.UI/Models/DatPatchModel.cs
@@ -52,13 +52,11 @@ namespace Core.UI.Models {
         }
 
         private void Messages_S2C_OnDDD_InterrogationMessage(object? sender, DDD_InterrogationMessage e) {
-            CoreUIPlugin.Log?.LogDebug("Patching start.");
             PatchPercentage = 0.1f;
             _net.Messages.S2C.OnDDD_InterrogationMessage -= Messages_S2C_OnDDD_InterrogationMessage;
         }
 
         private void Messages_S2C_OnDDD_OnEndDDD(object? sender, DDD_OnEndDDD e) {
-            CoreUIPlugin.Log?.LogDebug("Patching done.");
             PatchPercentage = 1f;
             _net.Messages.S2C.OnDDD_OnEndDDD -= Messages_S2C_OnDDD_OnEndDDD;
         }
@@ -66,26 +64,14 @@ namespace Core.UI.Models {
         private void Net_OnS2CPacket(object? sender, PacketEventArgs e) {
             if (e.Packet.Header.Flags.HasFlag(MagicHat.ACProtocol.Enums.PacketHeaderFlags.ConnectRequest)) {
                 ConnectPercentage = 1f;
-                //_net.OnS2CPacket -= Net_OnS2CPacket;
-            }
-
-            if (e.Packet.Messages is not null) {
-                foreach (var msg in e.Packet.Messages) {
-                    CoreUIPlugin.Log?.LogDebug("Got message from server: " + msg.OpCode.ToString("X8"));
-                }
+                _net.OnS2CPacket -= Net_OnS2CPacket;
             }
         }
 
         private void Net_OnC2SPacket(object? sender, PacketEventArgs e) {
             if (e.Packet.Header.Flags.HasFlag(MagicHat.ACProtocol.Enums.PacketHeaderFlags.LoginRequest)) {
                 ConnectPercentage = 0.5f;
-                //_net.OnC2SPacket -= Net_OnC2SPacket;
-            }
-
-            if (e.Packet.Messages is not null) {
-                foreach (var msg in e.Packet.Messages) {
-                    CoreUIPlugin.Log?.LogDebug("Got message TO server: " + msg.OpCode.ToString("X8"));
-                }
+                _net.OnC2SPacket -= Net_OnC2SPacket;
             }
         }
 
