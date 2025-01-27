0888c97b63135456d4fc84b1317717fe572e0c3d
trevis <trevis@haxit.org>
2025-01-27 00:47:37 -0700

    remove rmlui

--- ext/RmlUi.Native/src/RenderManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "RmlNative.h"
-#include "RmlUi/Core.h"
-#include "EventListener.h"
-#include <string>
-
-//RMLUI_CAPI Rml::Texture rml_RenderManager_LoadTexture(Rml::RenderManager* renderManager, const char* source, const char* document_path) {
-    //return Rml::GetRenderInterface().LoadTEx
-//}
--- ext/RmlUi.Native/src/Vector.cpp
+++
@@ -1,4 +0,0 @@
-#include "RmlNative.h"
-#include "RmlUi/Core.h"
-#include <string>
-#include <unordered_map>
